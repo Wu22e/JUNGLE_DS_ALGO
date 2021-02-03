9e639bee715c768f7d5159174123a1a6ea1e4b8d
Wu22e <gulb1602@gmail.com>
2021-02-03 19:50:28 +0900

    add I/O problems

--- Additional Problem/B1[1924] 2007년.py
+++ Additional Problem/B1[1924] 2007년.py
@@ -1,8 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-last_day = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
+last_day = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+day_of_week = ["MON","TUE","WED","THU","FRI","SAT","SUN"]
 x, y = map(int, input().split())
 
-change_day = x*last_day[x]
+change_day = 0
+
+for i in range(x-1):
+    change_day += last_day[i]
+change_day += y - 1
+
+
+print(day_of_week[change_day%7])
